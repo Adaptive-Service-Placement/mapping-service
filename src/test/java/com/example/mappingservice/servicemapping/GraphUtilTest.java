0025e9c40dc268f3b35893abebcfdbf3d17f9545
fsix <six@campus.tu-berlin.de>
2023-03-02 19:57:02 +0100

    added testNG dependency, added GraphUtilTest.java

---
+++ src/test/java/com/example/mappingservice/servicemapping/GraphUtilTest.java
@@ -0,0 +1,49 @@
+package com.example.mappingservice.servicemapping;
+
+import com.example.mappingservice.ApplicationSystem;
+import com.example.mappingservice.Service;
+import com.example.mappingservice.util.GraphUtil;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GraphUtilTest {
+
+    private GraphUtil underTest;
+
+    @BeforeEach
+    public void setUp() {
+        underTest = new GraphUtil();
+    }
+
+    @Test
+    public void constructGraphFromSystemTest() {
+        assertGraph(null, null);
+    }
+
+    @DataProvider
+    private Object[][] performMinKCutAlgorithmDataProvider() {
+        return new Object[][]{
+                {},
+                {},
+                {},
+                {}
+        };
+    }
+
+    @Test(dataProvider = "performMinKCutAlgorithmDataProvider")
+    public void performMinKCutAlgorithmTest() {
+        int k = 2;
+        assertEquals(null, underTest.performMinKCutAlgorithm(null, k));
+    }
+
+    private void assertGraph(ApplicationSystem applicationSystem, SimpleWeightedGraph<Service, DefaultWeightedEdge> graph) {
+        // TODO: assert expected edges, vertices, weights
+        assertEquals(graph, underTest.constructGraphFromSystem(applicationSystem));
+    }
+
+}
