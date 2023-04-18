33d0fef7f5867b30418bbb968f957e88788e487d
fsix <six@campus.tu-berlin.de>
2023-04-18 19:58:03 +0200

    added print statements for debugging purposes

--- src/main/java/com/example/mappingservice/consumer/MappingRequestConsumer.java
+++ src/main/java/com/example/mappingservice/consumer/MappingRequestConsumer.java
@@ -20,15 +20,30 @@ public class MappingRequestConsumer {
 
     @RabbitListener(queues = MessagingConfig.MAPPING_QUEUE)
     public void handleMappingRequest(ApplicationSystem applicationSystem) {
-        setAffinities(applicationSystem);
+        if (applicationSystem.getServices().size() > 1) {
+            System.out.println("Message received!");
+            System.out.println("The following system has been sent to me:");
+            System.out.println("All Services:");
+            applicationSystem.getServices().forEach(service -> System.out.println(service.getIpAdresse()));
+            System.out.println("All connections:");
+            applicationSystem.getConnections().forEach(connection -> System.out.println(connection.getService1().getIpAdresse() + "," + connection.getService2().getIpAdresse() + "; Weight: " + connection.getBytesExchanged()));
+            setAffinities(applicationSystem);
 
-        Mapper mapper = new Mapper(applicationSystem, applicationSystem.getNumberOfNodes());
+            Mapper mapper = new Mapper(applicationSystem, applicationSystem.getNumberOfNodes());
 
-        template.convertAndSend(MessagingConfig.INTERNAL_EXCHANGE, MessagingConfig.EXECUTE_MIGRATION_ROUTING_KEY, mapper.getMigrationInstruction());
+            System.out.println("Mapper has been successfully created.");
+
+            if (mapper.getMigrationInstruction() != null) {
+                System.out.println(mapper.getMigrationInstruction().getGroups());
+            }
+
+            template.convertAndSend(MessagingConfig.INTERNAL_EXCHANGE, MessagingConfig.EXECUTE_MIGRATION_ROUTING_KEY, mapper.getMigrationInstruction());
+        }
     }
 
-    private void setAffinities(ApplicationSystem appSystem) {
+    public void setAffinities(ApplicationSystem appSystem) {
         appSystem.getConnections().forEach(connection -> {
+            System.out.println("Computing affinity...");
             BigDecimal affinity = connection.getBytesExchanged().divide(appSystem.getBytesExchangedTotal(), 2, HALF_UP);
             connection.setAffinity(affinity);
         });
